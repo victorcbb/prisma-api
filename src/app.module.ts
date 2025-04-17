7c1b53dca2b3def54d01b02f5333138307269fbb
Victor <victorclovisb@hotmail.com>
2025-04-17 09:59:56 -0300

    feat: add PrismaService and corresponding tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
