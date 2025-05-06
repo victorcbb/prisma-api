1d576ef8533fec3915146e5b9bf1a35921046a3c
Victor <victorclovisb@hotmail.com>
2025-05-05 21:15:50 -0300

    feat: add PrismaClientError type for enhanced error handling

---
+++ src/common/errors/types/PrismaClientError.ts
@@ -0,0 +1,7 @@
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
+
+export type PrismaClientError = PrismaClientKnownRequestError & {
+  meta?: {
+    target?: string[];
+  };
+};
