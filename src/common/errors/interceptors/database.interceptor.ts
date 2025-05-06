7020e86a091b443184d4ac3ae0c023f52cf27810
Victor <victorclovisb@hotmail.com>
2025-05-05 21:58:21 -0300

    feat: enhance DatabaseInterceptor to handle Prisma errors and throw BadRequestException for DatabaseError

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -3,15 +3,26 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  BadRequestException,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { isPrismaError } from '../utils/is-prisma-error';
+import { handleDatabaseErros } from '../utils/handle-database-errors.util';
+import { DatabaseError } from '../types/databaseError';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
-        throw error;
+        if (isPrismaError(error)) {
+          error = handleDatabaseErros(error);
+        }
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        } else {
+          throw error;
+        }
       }),
     );
   }
