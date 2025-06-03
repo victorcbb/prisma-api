935728a4cd165a87b23faea9c32110f781f7b5d0
Victor <victorclovisb@hotmail.com>
2025-06-02 21:46:35 -0300

    fix: update CreatePostDto to replace authorEmail with authorId and adjust validation decorators

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
@@ -9,6 +9,6 @@ export class CreatePostDto {
   @IsOptional()
   content?: string;
 
-  @IsEmail()
-  authorEmail: string;
+  @IsNumber()
+  authorId: number;
 }
