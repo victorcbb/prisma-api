0bc80e9e1e541ba5839abfbf6a42d5f2a3215024
Victor <victorclovisb@hotmail.com>
2025-06-16 10:43:19 -0300

    docs: update import statement for PartialType to use '@nestjs/swagger'

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types'; // Utilizar esse import caso não esteja usando o plugin do Swagger
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
