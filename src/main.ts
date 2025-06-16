27483749bf1b6bce73218f5d9690b2bd4f0e02e9
Victor <victorclovisb@hotmail.com>
2025-06-16 10:21:07 -0300

    feat: setup Swagger documentation for NestJS Prisma API

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,22 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotFoundInterceptor } from './common/errors/interceptors/notFound.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Prisma API')
+    .setDescription(
+      'Application for studying the NestJS framework with Prisma ORM',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
